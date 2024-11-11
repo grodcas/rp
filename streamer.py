5b855409944582f93108aa51a0e2eefd29a2096c
grodcas <119622068+grodcas@users.noreply.github.com>
2024-11-11 22:10:21 +0100

    Update streamer.py

--- streamer.py
+++ streamer.py
@@ -1,4 +1,13 @@
 import cv2
+from picamera2 import Picamera2, Preview
+
+
+picam= Picamera2()
+
+config = picam.create_preview_configuration()
+picam.configure(config)
+picam.start()
+
 
 cap = cv2.VideoCapture(1)  # Force the V4L2 backend
 
